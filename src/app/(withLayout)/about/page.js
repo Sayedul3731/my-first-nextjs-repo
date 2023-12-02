d303053c6efc60986432017042c3c50228f83937
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-12-02 13:32:35 +0600

    image added

--- src/app/(withLayout)/about/page.js
+++ src/app/(withLayout)/about/page.js
@@ -1,9 +1,21 @@
+import Image from 'next/image';
 import React from 'react';
-
+import takiImage from "@/assets/taki.jpg"
+export const metadata = {
+    title: 'About', 
+    description: 'this is about page.'
+}
 const AboutPage = () => {
     return (
         <div>
             <h1>This is about page...</h1>
+
+            <Image src="https://i.ibb.co/Zxg7YNv/taki.jpg"
+            width={300}
+            height={300} alt="taki's image"/>
+            <Image src={takiImage}
+            width={300}
+            height={300} alt="taki's image"/>
         </div>
     );
 };
